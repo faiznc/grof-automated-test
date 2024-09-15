a850ac712f0222db4c5d385b7701e6a6fc060bfa
Faiz Noerdiyan Cesara <faiznc@gmail.com>
2024-09-15 10:26:39 +0700

    build: update test runner configs

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // default: `--require-module ts-node/register --require ./src/step-definitions/**/*.ts --require ./src/hooks/**/*.ts --format progress --publish-quiet`
-    default: `--require-module ts-node/register --require ./src/step-definitions/**/*.ts --require ./src/hooks/**/*.ts --format progress`
-  };
-  
+  format:['html:cucumber-report.html'],
+  parallel: 1,
+  default: "--require-module ts-node/register --require ./src/step-definitions/**/*.ts --require ./src/hooks/**/*.ts --format progress"
+};
